16ec0a08b721667341ca64bdcf7dcff39cd944da
Shivam-lalwani <shivamlalwani222@gmail.com>
2017-07-26 17:19:44 +0100

    OOPS3

---
+++ Reader.java
@@ -0,0 +1,191 @@
+/**
+ * 
+ * @author Shivam
+ * @Date: 26-07-2017
+ * Class Name: Reader 
+ * @description This class is having multiple methods. This class is first of all reading all three files and
+ * storing the data of all three in individual arrays to perform searching the attributes of product.
+ * This class is returning the desired values of product attributes to order class.
+ */
+package ProductList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reader {
+	//List<String> ProductList1  = new ArrayList<String>();
+	String[] ProductList = null;
+	String[] ProductPromo = null;
+	String[] OrderPromo = null;
+	
+	//three arrays for storing each column of ProductList.csv file in individual arrays
+	public static String codeOfProduct[]=new String[100];
+	public static String nameOfProduct[]=new String[100];
+	public static String priceOfProduct[]=new String[100];
+	
+	//three arrays for storing each column of ProductPromo.csv file in individual arrays
+	public static String thresholdPriceArray[]=new String[100];
+	public static String amountToBePromotedArray[]=new String[100];
+	public static String typeOfPromoArray[]=new String[100];
+	
+	//three arrays for storing each column of OrderPromo.csv file in individual arrays
+	public static String order_typeOfPromoArray[] = new String[100];
+	public static String orderAmountToBePromotedArray[] = new String[100];
+	public static String order_applicableAmountArray[] = new String[100];
+	
+	//counters to count the valid number of enteries in each file
+	int productArrayCounter = 0;
+	int productPromoArrayCounter = 0;
+	int orderPromoArrayCounter = 0;
+	
+	//lists for splitting the files and store them in arrays
+	List<String> ProductPromoList  = new ArrayList<String>();
+	List<String> OrderPromoList  = new ArrayList<String>();
+	List<String> lines  = new ArrayList<String>();
+	
+	/**
+	 * ReadFile method is for reading the content from each file and displaying the content of each file
+	 * on console according to requirement.
+	 * @param filepath, the path of each file is passed at calling method
+	 */
+	public void ReadFile(String filepath) {
+		BufferedReader Reader = null;
+		try {
+			Reader = new BufferedReader(new FileReader(filepath)); //Reader object to read file's data
+		} catch (FileNotFoundException e) {	//catch block to handle the eception, if the file not found or their is any problem in fetching the file
+			System.out.print("Error in fetching file");	//desired error to be displayed
+			e.printStackTrace();
+		}
+		
+		/**
+		 * reading the file whose filepath is passed in parameter filepath
+		 */
+		try {
+			String lineData = " ";	//string to store line by line data of a file and diplaying that to console 
+			while ((lineData = Reader.readLine()) != null) {
+				System.out.println(lineData);
+				if(filepath=="E:\\ProductList.csv") {
+				
+					ProductList = lineData.split(",");
+					String code = ProductList[0];
+					codeOfProduct[productArrayCounter] = code;	//maintaining productCode Array
+					
+					String name = ProductList[1];
+					nameOfProduct[productArrayCounter]=name;	//maintaining productName Array
+					
+					String price = ProductList[2];
+					priceOfProduct[productArrayCounter]=price;	//maintaining productPrice Array
+					productArrayCounter++;
+				}
+				
+				if(filepath=="E:\\ProductPromo.csv") {
+					
+					ProductPromo = lineData.split(",");
+					
+					String typeOfPromo = ProductPromo[0];
+					typeOfPromoArray[productPromoArrayCounter] = typeOfPromo;		//maintaining type of productDiscountToBeApplied Array
+					
+					String amountToBePromoted = ProductPromo[1];
+					amountToBePromotedArray[productPromoArrayCounter]=amountToBePromoted;	//maintaining value of productDiscountToBeApplied Array
+					
+					String thresholdPrice = ProductPromo[2];
+					thresholdPriceArray[productPromoArrayCounter]=thresholdPrice;	//maintaining codes on which discount is to be applied
+					productPromoArrayCounter++;
+				}
+
+				if(filepath=="E:\\OrderPromo.csv") {
+					
+					OrderPromo = lineData.split(",");
+					
+					String order_typeOfPromo = OrderPromo[0];
+					order_typeOfPromoArray[orderPromoArrayCounter] = order_typeOfPromo;	//maintaining type of orderDiscountToBeApplied Array
+					
+					String order_amountToBePromoted = OrderPromo[1];
+					orderAmountToBePromotedArray[orderPromoArrayCounter]=order_amountToBePromoted;	//maintaining value of orderDiscountToBeApplied Array
+					
+					String order_applicableAmount = OrderPromo[2];
+					order_applicableAmountArray[orderPromoArrayCounter]=order_applicableAmount;	//maintaining price of orderDiscountToBeApplied Array
+					orderPromoArrayCounter++;
+				}
+				lines.add(lineData);
+			}
+			Reader.close();
+		} catch (IOException e) {
+			System.out.print("Error");
+			e.printStackTrace();
+		}
+		
+	}
+	/**
+	 * 
+	 * @param productCode
+	 * @param productQuantity
+	 * @return the product of price and quantity of product
+	 */
+	public int searchDetail(String productCode,int productQuantity) {
+		for(int k=0; k<codeOfProduct.length; k++) {
+			if(codeOfProduct[k].equals(productCode)) {
+				return Integer.parseInt(priceOfProduct[k])*productQuantity;
+			}
+		}
+	   return 0;
+	}
+	/**
+	 * 
+	 * @param productCode
+	 * @return the name of product
+	 */
+	public String searchName(String productCode) {
+		for(int k=0; k<codeOfProduct.length; k++) {
+			if(codeOfProduct[k].equals(productCode)) {
+				return nameOfProduct[k];
+			}
+		}
+	   return "Product not Found";
+	}
+	/**
+	 * 
+	 * @param priceWithoutDiscount, the subtotal amount without discount
+	 * @return the discounted price according to order price
+	 */
+	public int isApplicableOrderPromo(int priceWithoutDiscount) {
+		for(int k=orderPromoArrayCounter; k>=0; k--) {
+			if(priceWithoutDiscount >= Integer.parseInt((order_applicableAmountArray[k]))) {
+				if((order_typeOfPromoArray[k]).equalsIgnoreCase("OrderFixedAmountPromotion"))
+				return (priceWithoutDiscount-Integer.parseInt(orderAmountToBePromotedArray[k]));
+				if((order_typeOfPromoArray[k]).equalsIgnoreCase("OrderFixedPercentPromotion"));
+				System.out.println(priceWithoutDiscount-((priceWithoutDiscount*Integer.parseInt(orderAmountToBePromotedArray[k]))/100));
+				return (priceWithoutDiscount-((priceWithoutDiscount*Integer.parseInt(orderAmountToBePromotedArray[k]))/100));
+			}
+		}
+		return priceWithoutDiscount;
+	}
+	/**
+	 * 
+	 * @param tempTotalWithoutDiscount, the subtotal amount without discount
+	 * @param productCode
+	 * @return the discounted price according to product promo file
+	 */
+	public int isApplicableProductPromo(int tempTotalWithoutDiscount, String productCode) {
+		for(int k=productPromoArrayCounter; k>=0; k--) {
+			if((thresholdPriceArray[k]).contains(productCode)) {
+				if((typeOfPromoArray[k]).equalsIgnoreCase("ProductFixedAmountPromotion"))
+				return (tempTotalWithoutDiscount-Integer.parseInt(amountToBePromotedArray[k]));
+				if((typeOfPromoArray[k]).equalsIgnoreCase("ProductFixedPercentPromotion"));
+				return (tempTotalWithoutDiscount-(((tempTotalWithoutDiscount*Integer.parseInt(amountToBePromotedArray[k]))/100)));
+			}
+		}
+		return tempTotalWithoutDiscount;
+	}
+	public static boolean checkProductCode(String productCode) {
+		for(int k=0; k<codeOfProduct.length; k++) {
+			if(codeOfProduct[k].equals(productCode)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ ShoppingApp.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ * @author Shivam
+ * @Date: 26-07-2017
+ * Class Name: ShoppingApp 
+ * @description This class is a Bill generating Class for an organization.
+ * The number of order is entered by user and code of product and quantity of those prodes also.
+ * And the discounted amount is displayed to console after applied different types of discounts.
+ * The Product details and types of discounts are fetched through 3 files :-
+ * 1. Product List - File containing product code, product name, and product price.
+ * 2. Promotion on total value - File containing type of Discount and threshold values.
+ * 3. Promotion on particular products - File containing type of discount and product code on which the
+ * 	  discount is to be applied.
+ */
+
+package ProductList;
+import java.util.Scanner;
+/*
+ * Main class that takes number of orders, quantity and code of products from user.
+ * And displays the subtotal, discounted and total amount.
+ */
+public class ShoppingApp {
+	static String productCode;	//code of Particular Product
+	static int quantity;	//quantity of Particular Product
+	static int tempTotal=0,discountedPrice=0,totalDiscountedPrice=0,total=0;	//temporary total value and discounted price
+	static String Name=" ";	//name of Particular Product
+	public static void main(String[] args) throws Exception{
+		/*object of reader class to read all files and display those on console one by one
+		 * and provide the details of code, price, type of discount etc.
+		*/
+		Reader read = new Reader();
+		System.out.print("LIST OF PRODUCTS"+"\n");
+		read.ReadFile("E:\\ProductList.csv");
+		System.out.print("LIST OF DISCOUNTS"+"\n");
+		read.ReadFile("E:\\ProductPromo.csv");
+		System.out.print("LIST OF DISCOUNTS"+"\n");
+		read.ReadFile("E:\\OrderPromo.csv");
+
+		Order fetchDetail = new Order();	//object of Order class
+		Product prod = new Product();	//object of Product class
+		System.out.println("\n"+"Enter number of Products");
+		Scanner sc = new Scanner(System.in);
+		int noOfProducts = sc.nextInt();
+		if(noOfProducts<=0) {
+			throw new Exception("Enter a valid number");
+		}
+		//take number of products, user want to buy and iterate this loop for the process
+		for(int count=0; count < noOfProducts; count++) {
+			System.out.println("\n"+"Enter code");
+			productCode = sc.next();	//getting code of product one by one from user
+			if(Reader.checkProductCode(productCode)==true) {
+				throw new Exception("Enter a valid code");
+			}
+			prod.setProductCode(productCode);	//saving productCode to Product Class by Product Class object
+			System.out.println("\n"+"Enter quantity");
+			quantity = sc.nextInt();	//getting code of quantity of each product one by one from user
+			if(quantity<=0) {
+				throw new Exception("Enter a valid quantity");
+			}
+			prod.setProductQuantity(quantity);	//saving productQuantity to Product Class by Product Class object
+			//tempTotal is holding temporary total values of each product and adding to next products price
+			tempTotal = fetchDetail.searchDetails(prod);
+			total = total+tempTotal;	//if products more than 1
+			//According to productCode entered by user, variable Name is fetching its name from this method call
+			Name += fetchDetail.searchName(prod);	//if products more than 1
+			//According to productCode entered by user and subtotal, variable Name is fetching its name from this method call
+			discountedPrice = fetchDetail.isApplicable(tempTotal,prod.getProductCode());
+			totalDiscountedPrice += discountedPrice;
+		}
+		/*
+		 * Display Bill after applying all the discounts on each order 
+		 */
+		System.out.print("\n\n"+"###################################################"+"\n");
+		System.out.print("                 Your Order"+"\n");
+		System.out.print("###################################################"+"\n"+"\n");
+		System.out.print("PURCHASED PRODUCTS"+"\n");
+		System.out.print("Product: "+Name +"\n");
+		System.out.print("SubTotal: INR "+total+"\n");
+		System.out.print("Discounted Applied: INR " +(total-totalDiscountedPrice)+"\n");
+		System.out.print("Total :  INR " +totalDiscountedPrice+"  "+"\n");
+		sc.close();
+	}
+}
