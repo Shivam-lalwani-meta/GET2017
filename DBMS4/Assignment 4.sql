87dc2fc42a4a5430e0e8cad4fb0288f937f0c817
Shivam-lalwani-meta <shivam.lalwani@metacube.com>
2017-09-07 11:46:47 +0100

    Update Assignment 4.sql

--- DBMS4/Assignment 4.sql
+++ DBMS4/Assignment 4.sql
@@ -30,7 +30,7 @@ Select * FROM Show_details;
 
 
 SELECT member_id, member_name, CASE WHEN category = "S" THEN 'Student' WHEN category = "F" THEN 'Faculty' 
-ELSE 'Others' END AS re_type_text
+ELSE 'Others' END AS category
 FROM Members; 
 
 
@@ -38,10 +38,10 @@ CREATE VIEW Show_Full_details AS
 
 SELECT s.subject_name, t.title_name, m.member_name, m.category,bi.member_id, bi.issue_date, bi.due_date, bi.accession_no, br.return_date 
 FROM Book_issue bi
-LEFT OUTER JOIN Book_return br ON
+LEFT JOIN Book_return br ON
 bi.accession_no = br.accession_no AND
 bi.member_id = br.member_id
-RIGHT OUTER JOIN Members m ON
+LEFT JOIN Members m ON
 m.member_id = bi.member_id
 INNER JOIN Books b ON
 bi.accession_no = b.accession_no
@@ -51,9 +51,3 @@ INNER JOIN Subjects s ON
 t.subject_id = s.subject_id;
 
 Select * FROM Show_Full_details;
-
-/*SELECT bi.member_id, bi.accession_no, bi.issue_date, br.return_date, m.member_name FROM Book_issue bi
-LEFT JOIN Book_return br ON
-bi.accession_no = br.accession_no
-RIGHT OUTER JOIN Members m ON
-m.member_id = bi.member_id;*/
