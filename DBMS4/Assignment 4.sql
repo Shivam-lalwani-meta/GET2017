c734d7dfe44eb4d5c4e7fb8857c43607be21b2e5
Shivam-lalwani-meta <shivam.lalwani@metacube.com>
2017-09-19 09:58:52 +0100

    Update Assignment 4.sql

--- DBMS4/Assignment 4.sql
+++ DBMS4/Assignment 4.sql
@@ -1,11 +1,13 @@
 
 
-SELECT * FROM Members WHERE category = (SELECT category FROM Members WHERE member_name = "Shivam Lalwani");
+SELECT member_name FROM Members WHERE category = (SELECT category FROM Members WHERE member_name = "Shivam Lalwani");
 
-SELECT member_id, issue_date, due_date FROM Book_issue
-where member_id NOT IN (SELECT member_id FROM Book_return WHERE Book_issue.member_id = Book_return.member_id);
+SELECT M.member_name, bi.issue_date, bi.due_date FROM Book_issue bi
+INNER JOIN Members M ON
+M.member_id = bi.member_id
+where M.member_id NOT IN (SELECT member_id FROM Book_return WHERE bi.member_id = Book_return.member_id);
 
-SELECT Book_issue.member_id, m.member_name, issue_date, due_date, Titles.title_name FROM Book_issue
+SELECT m.member_name, issue_date, due_date, Titles.title_name FROM Book_issue
 INNER JOIN Members m ON
 m.member_id = Book_issue.member_id
 INNER JOIN Books ON
@@ -16,11 +18,9 @@ where not exists (SELECT member_id FROM Book_return WHERE Book_issue.member_id =
 
 SELECT * FROM Books WHERE price = (SELECT MAX(price) FROM Books)  AND purchase_date IS NOT NULL;
 
-SELECT * FROM Books ORDER BY price DESC LIMIT 2,1;
-
 SELECT accession_no, MAX(price) AS Second_Max_Price FROM Books WHERE price < (SELECT MAX(price) FROM Books);
 
-CREATE VIEW Show_details AS
+CREATE OR REPLACE VIEW Show_details AS
 
 SELECT m.member_name, bi.issue_date, bi.due_date, bi.accession_no FROM Members m
 INNER JOIN Book_issue bi ON
@@ -29,12 +29,12 @@ m.member_id = bi.member_id;
 Select * FROM Show_details;
 
 
-SELECT member_id, member_name, CASE WHEN category = "S" THEN 'Student' WHEN category = "F" THEN 'Faculty' 
+SELECT member_id, member_name, CASE category WHEN "S" THEN 'Student' WHEN  "F" THEN 'Faculty' 
 ELSE 'Others' END AS category
 FROM Members; 
 
 
-CREATE VIEW Show_Full_details AS
+CREATE OR REPLACE VIEW Show_Full_details AS
 
 SELECT s.subject_name, t.title_name, m.member_name, m.category,bi.member_id, bi.issue_date, bi.due_date, bi.accession_no, br.return_date 
 FROM Book_issue bi
